e78bbc7b81866558fd4d0cb344498d4b9c0fb2d7
Hae-Riri <ehl3288@naver.com>
2021-01-05 15:46:48 +0900

    롬복 기능 테스트

---
+++ src/main/java/com/ehl3288/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.ehl3288.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
